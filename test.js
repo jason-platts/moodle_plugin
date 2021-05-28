aa4375cbd600d1bb87d7113d9465840d955018ce
Jason Platts <j_platts@yahoo.co.uk>
2021-05-28 13:52:08 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-document.domain='open.ac.uk';
+window.addEventListener("message", function(event) {
+  if (event.origin.idexOf('open.ac.uk') === -1) {
+    // something from an unknown domain, let's ignore it
+    return;
+  }
+  if (event.data.indexOf('getHeight') === 0) {
+    var wrap = document.getElementById('wrap');
+    if (wrap) {
+      var height = wrap.clientHeight;
+      if (height) {
+        event.source.postMessage('updateHeight:' + height, event.origin);
+      }
+    }
+  }
+});
