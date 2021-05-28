bf0c17cfe9e65c21e69a2400a2d52d8975a4585e
Jason Platts <j_platts@yahoo.co.uk>
2021-05-28 15:19:51 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-window.addEventListener("message", function(event) {
+window.parent.addEventListener("message", function(event) {
   if (event.origin.indexOf('open.ac.uk') === -1) {
     // something from an unknown domain, let's ignore it
     return;
   }
-  if (event.data.indexOf('getHeight') === 0) {
-    var wrap = document.getElementById('wrap');
-    if (wrap) {
-      var height = wrap.clientHeight;
+  if (typeof event.data === 'string' && event.data.indexOf('getHeight') === 0) {
+    var contain = window.parent.document.getElementById('mediaContainer');
+    if (contain) {
+      var height = contain.clientHeight;
       if (height) {
         event.source.postMessage('updateHeight:' + height, event.origin);
       }
